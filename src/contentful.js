cf4747b921c99bc114537182541b5a5d2739a095
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2020-04-20 16:28:09 +0800

    Environment variables

--- src/contentful.js
+++ src/contentful.js
@@ -4,3 +4,8 @@ export default createClient ({
     space:"3l5u4b4rczvr",
     accessToken: "KQLlkbl_lpET4bt1GnFxeXjeMIcd81rcQD1ZM76UBGg"
 })
+
+
+
+// REACT_APP_API_SPACE = 3l5u4b4rczvr
+// REACT_APP_ACCESS_TOKEN = KQLlkbl_lpET4bt1GnFxeXjeMIcd81rcQD1ZM76UBGg
